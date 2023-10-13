b5bd8454a129bdd63def1feff45f42b4bcfb4c9f
sijuz <sijuz@yandex.ru>
2023-10-14 00:35:48 +0300

    fix bot

--- src/bot.ts
+++ src/bot.ts
@@ -15,14 +15,6 @@ const dataTg = {
     ] 
 }
 
-function getKeyb (username: string) {
-    return {
-        inline_keyboard: [
-            [ {text: 'Оплатить 10 TON', url: 'ton://transfer/UQAXT2kFyCKUT9Hz8tpgbPo8-Qturr_tp_ynSO4juL6N4omn?amount=10000000000&message='+username} ]
-        ] 
-    }
-}
-
 
 
 const keyboardAddAddress = {inline_keyboard: [
@@ -89,100 +81,6 @@ async function registerUserInDb (pool: DB, username: string | undefined, id_tele
     }
     // console.log(res)
     return res
-}
-
-async function registerUserInDb2 (pool: DB, username: string | undefined, id_telegram: number) {
-    // const wallet = new CustomWallet().newWallet()
-    let res = await pool.getUser2FromUserName(username ?? 'a')
-    if (res === undefined) {
-        res = await pool.getUser2(id_telegram)
-    }
-
-    if (res === undefined) {
-        await pool.addUser2({
-            id: 0,
-            username,
-            id_telegram,
-            pay: 0,
-            name_user: ''
-        } as User2)
-
-        const res2 = await pool.getUser2(id_telegram)
-        console.log(res2)
-        return res2
-    }
-    // console.log(res)
-    return res
-}
-
-async function updateTransfer(pool: DB, bot: TelegramBot) {
-    const _url2: string = 'https://tonapi.io/v2/'
-    const _token: string = 'AFXRKLZM2YCJ67AAAAAE4XDRSACSYEOYKQKOSUVUKMXNMP2AKUTWJ2UVBPTTQZWRGZMLALY'
-    const _address: string = 'UQAXT2kFyCKUT9Hz8tpgbPo8-Qturr_tp_ynSO4juL6N4omn'
-
-    while (true) {
-        await sleep(1000)
-
-        const res = await axios.get(`${_url2}blockchain/accounts/${_address}/transactions?limit=20`, { headers: { Authorization: `Bearer ${_token}` } })
-
-        if (res.data.error) {
-            console.error(res.data.result)
-            // return undefined
-        } else {
-            const trans = res.data.transactions
-
-            // console.log(res.data)
-
-            if (trans) {
-                for (let i=0; i< trans.length;i++) {
-                    if (trans[i].in_msg && trans[i].in_msg.decoded_body && trans[i].in_msg.decoded_body.text) {
-                        const username = trans[i].in_msg.decoded_body.text.replace('@', '')
-                        // console.log('username', username)
-                        // console.log('trans[i].in_msg.value', trans[i].in_msg.value)
-                        if (trans[i].in_msg.value >= 10000000000n) {
-
-                            let send = false
-                            const id = Number(username)
-                            if (id > 1) {
-
-                                const user = await pool.getUser2(id)
-                                if (user) {
-                                    if (user.pay === 0) {
-                                        await pool.updateUserPayFromId(id)
-
-                                        bot.sendMessage(user.id_telegram, 'Успешная оплата' )
-
-                                        send = true
-                                    }
-                                }
-                                
-                            } else {
-                                const user = await pool.getUser2FromUserName(username)
-
-                                if (user) {
-                                    if (user.pay === 0) {
-                                        await pool.updateUserPayFromUsername(username)
-
-                                        bot.sendMessage(user.id_telegram, 'Успешная оплата' )
-
-                                        send = true
-                                    }
-                                }
-                                
-                            }
-
-                            if (send) {
-
-                                bot.sendMessage(232885094, `Новая оплата\n\nСумма: ${trans[i].in_msg.value / 10 ** 9} TON\n\nНик: @${username}` )
-                                bot.sendMessage(238211251, `Новая оплата\n\nСумма: ${trans[i].in_msg.value / 10 ** 9} TON\n\nНик: @${username}` )
-                            }
-                        }
-
-                    }
-                }
-            }
-        }
-    }
 }
 
 async function startBot() {
@@ -255,67 +153,10 @@ async function startBot() {
     })
 }
 
-async function startBotTest () {
-    const connect = await new MySql().sync()
-
-    const db = new DB(connect.pool)
-
-    const token = '5898239617:AAHJAYyRptVSVNQqX9rlX49ZCxu1iBa3H-E'
-    const bot = new TelegramBot(token, { polling: true })
-
-    updateTransfer(db, bot)
-
-    bot.on('message', async (msg: TelegramBot.Message) => {
-        const chatId = msg.chat.id
-        const username = msg.chat.username ?? chatId + ''
-    
-        const userText = msg.text
-
-        const commands = userText?.split(' ')
-
-        await registerUserInDb2(db, username, chatId)
-
-        if (commands && (username === 'some_wallet' || username === 'sijuz') && commands.length > 0 && commands[0] === '/send') {
-            const users = await db.getAllUsers2()
-            for (let i=0; i<users.length;i++) {
-                bot.sendMessage(users[i].id_telegram, userText?.replace('/send ', '') ?? '', { 
-                    parse_mode: 'HTML'
-                })
-            }
-
-            bot.sendMessage(chatId, 'Рассылка успешно отправлена на ' + users.length + ' пользователей', { 
-                parse_mode: 'HTML'
-            })
-        } else if (commands && (username === 'some_wallet' || username === 'sijuz') && commands.length > 0 && commands[0] === '/users') {
-            const users = await db.getAllUsers2()
-
-            let text = "Все пользльзователи("+users.length+"):\n\n"
-
-            for (let i=0; i<users.length;i++) {
-                text = text + '#' + users[i].id_telegram + ' @' +  users[i].username + ' ' + (users[i].pay === 1 ? '🟢\n' : '🟡\n')
-                
-            }
-
-            bot.sendMessage(chatId, text, { 
-                parse_mode: 'HTML'
-            })
-        } else {
-
-    
-            bot.sendMessage(chatId, 'Привет это бот для оплаты', { 
-                parse_mode: 'HTML',
-                reply_markup: getKeyb(username ?? chatId + '')
-            })
-        }
-        
-    })
-}
-
 
 
 app.listen(port, () => {
 
     startBot()
-    // startBotTest()
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 })
